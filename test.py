6b04d450b3d7b12a86ef03be09f1e8f1a809c4cf
daesang1 <gongdaewang@gmail.com>
2018-01-04 01:36:36 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+import os
+
+print(os.listdir("dir"))
+if ()
+F = open("tsst.txt", "w")
+W = os.listdir("dir") 
+for x in range(len(W)):
+    F.write(W[x] + "\n")
+
+try:
+    os.system("git add tsst.txt")
+    os.system("git commit -m ""new""")
+finally:
+    pass
+    
+
